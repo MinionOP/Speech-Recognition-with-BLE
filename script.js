4de3582d533153d1908b3a9f23ae3d617dcd8a34
MinionOP <73415249+MinionOP@users.noreply.github.com>
2021-04-13 01:04:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,60 @@
-var message = document.querySelector('#message');
+
+/*
+const text = document.querySelector('.text');
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecongition;
+const recognition = new window.SpeechRecognition();
+
+
+recognition.interimResults = true;
+
+
+let p = document.createElement('p');
+recognition.addEventListener('result',(e)=>{
+    console.log(e);
+    })
+recognition.start();
+
+*/
+
+/*
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-var SpeechGrammerList = SpeechGrammarList || webkitSpeechGrammerList; 
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
 
 var grammer = '#JSGF V1.0;';
 
 var recognition = new SpeechRecognition();
-var speechRecognitionGrammerList = new SpeechGrammerList();
-
-speechRecognitionGrammerList.addFromString(grammer, 1);
-
-recognition.grammers = speechRecognitionGrammerList;
+var SpeechRecognitionGrammerList = new SpeechGrammarList();
+SpeechRecognitionGrammerList.addFromString(grammer,1);
+recognition.grammars = SpeechRecognitionGrammerList;
 recognition.lang = 'en-US';
 recognition.interimResults = false;
+
+
+recognition.onspeechend = function(){
+    recognition.stop();
+}
+
+recognition.onerror = function(event){
+
+}
+*/
+
+
+
+function record(){
+    var recognition = new webkitSpeechRecognition();
+    recognition.lang = 'en-US';
+
+    recognition.onspeechend = function(){
+        recognition.stop();
+    }
+
+    recognition.onresult = function(event){
+        console.log(event);
+        document.getElementById('text').innerText = event.results[0][0].transcript;
+    }
+    recognition.start();
+}
 
