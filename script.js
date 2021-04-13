069e2d059d3c7b96d3f8a86b5985e3689a837a40
MinionOP <73415249+MinionOP@users.noreply.github.com>
2021-04-13 17:13:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-
-/*
-const text = document.querySelector('.text');
-
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecongition;
-const recognition = new window.SpeechRecognition();
-
-
-recognition.interimResults = true;
-
-
-let p = document.createElement('p');
-recognition.addEventListener('result',(e)=>{
-    console.log(e);
-    })
-recognition.start();
-
-*/
-
-/*
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
 var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
@@ -32,29 +12,41 @@ recognition.lang = 'en-US';
 recognition.interimResults = false;
 
 
+recognition.onresult = function(event){
+    console.log(event);
+    var message = event.results[0][0].transcript;
+    document.getElementById('paragraphText').innerHTML = message;
+
+    if(message.toLowerCase() === 'light on'){
+        alert("Living room light is on")
+    }
+    else if(message.toLowerCase() === 'light off'){
+        alert('Living room light is off')
+    }
+    else if(message.toLowerCase() === 'open garage'){
+        alert('Opening garage')
+    }
+    else if(message.toLowerCase() === 'close garage'){
+        alert('Closing garage')
+    }
+};
+
+
 recognition.onspeechend = function(){
+    document.getElementById('recording').style.display = "none";
+    document.getElementById('button1').style.display = "block";
     recognition.stop();
-}
+};
 
 recognition.onerror = function(event){
-
+    document.getElementById('recording').style.display = "none";
+    document.getElementById('button1').style.display = "block";
+    document.getElementById('paragraphText').innerHTML = 'Error occurred in recognition: ' + event.error;
 }
-*/
 
-
-
-function record(){
-    var recognition = new webkitSpeechRecognition();
-    recognition.lang = 'en-US';
-
-    recognition.onspeechend = function(){
-        recognition.stop();
-    }
-
-    recognition.onresult = function(event){
-        console.log(event);
-        document.getElementById('text').innerText = event.results[0][0].transcript;
-    }
+document.querySelector('#button1').addEventListener('click', function(){
+    document.getElementById('button1').style.display = "none";
+    document.getElementById('recording').style.display = "block";
     recognition.start();
-}
+});
 
