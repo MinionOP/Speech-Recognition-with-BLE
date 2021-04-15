4abfb4566cb4b7f68754903b717a9c154c782a27
MinionOP <73415249+MinionOP@users.noreply.github.com>
2021-04-14 20:09:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -18,16 +18,16 @@ recognition.onresult = function(event){
     document.getElementById('paragraphText').innerHTML = message;
 
     if(message.toLowerCase() === 'light on'){
-        alert("Living room light is on")
+        alert("Living room light is on");
     }
     else if(message.toLowerCase() === 'light off'){
-        alert('Living room light is off')
+        alert('Living room light is off');
     }
     else if(message.toLowerCase() === 'open garage'){
-        alert('Opening garage')
+        alert('Opening garage');
     }
     else if(message.toLowerCase() === 'close garage'){
-        alert('Closing garage')
+        alert('Closing garage');
     }
 };
 
@@ -50,3 +50,84 @@ document.querySelector('#button1').addEventListener('click', function(){
     recognition.start();
 });
 
+
+
+//-----------------Bluetooth------------------------
+let characteristicCache = null;
+let deviceCache = null;
+
+function connect(){
+    navigator.bluetooth.requestDevice({
+        filters:
+        [
+            {services: [0xFFE0]}
+        ]
+    })
+    .then(device =>{
+        console.log('Name: ' +device.name);
+        deviceCache = device;
+        return device.gatt.connect();
+    })
+    .then(server =>{
+        console.log('Getting GATT Service...')
+        return server.getPrimaryService(0xFFE0);
+    })
+    .then(service =>{
+        console.log('Getting GATT Characteristic...')
+        return service.getCharacteristic(0xFFE1);
+    })
+    .then(characteristic =>{
+        console.log('Found characteristic');
+        characteristicCache  = characteristic;
+    })
+    .catch(error =>{
+        console.log('Request device error: ' + error);
+    })
+    
+}
+
+
+document.querySelector('#connectDevice').addEventListener('click', function(event){
+    event.stopPropagation();
+    event.preventDefault();
+
+    if(navigator.bluetooth){
+        console.log('Web Bluetooth is available');
+        connect();
+    }
+    else{
+        console.log('Web Bluetooth is available');
+    }
+});
+
+document.querySelector('#disconnectDevice').addEventListener('click', function(event){
+    if(deviceCache){
+        if(deviceCache.gatt.connected){
+            deviceCache.gatt.disconnect();
+            console.log(deviceCache.name + ' disconnected');
+        }
+        else{
+            console.log(deviceCache.name +' is already disconnected');
+        }
+    }
+    characteristicCache = null;
+    deviceCache = null;
+});
+
+document.querySelector('#onTest').addEventListener('click', function(event){
+    if(characteristicCache){
+        characteristicCache.writeValue(new TextEncoder().encode('1'));   
+    }
+    else{
+        console.log('No characteristic');
+    }
+});
+
+document.querySelector('#offTest').addEventListener('click', function(event){
+    if(characteristicCache){
+        characteristicCache.writeValue(new TextEncoder().encode('0'));   
+    }
+    else{
+        console.log('No characteristic');
+    }
+});
