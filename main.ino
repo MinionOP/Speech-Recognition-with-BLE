fdd8b793d0f2e3a10165f75e3f831513c8c3b983
MinionOP <73415249+MinionOP@users.noreply.github.com>
2021-04-26 09:53:50 -0500

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,20 +1,38 @@
+#include <Arduino_FreeRTOS.h>
+#include <semphr.h>
+
 #include <SoftwareSerial.h>
 #include <Servo.h>
 SoftwareSerial HM10(10, 11);  //[RX, TX]
 
 Servo myServo;
 char msg;
+SemaphoreHandle_t xBinarySemaphore;
 
 
 void setup()
 {
+  
     Serial.begin(9600);
-    myServo.attach(3);
-    pinMode(8, OUTPUT);
     HM10.begin(9600);
+    xBinarySemaphore = xSemaphoreCreateBinary();
+    
+    xTaskCreate(TaskReadHM10, // Task function
+              "ReadHM10", // Task name
+              128, // Stack size 
+              NULL, 
+              0 ,// Priority
+              NULL );
+   xTaskCreate(TaskAction, // Task function
+              "Action", // Task name
+              128, // Stack size 
+              NULL, 
+              0, // Priority
+              NULL );
+    
 }
-
 void loop(){
+  /*
     if (HM10.available()) {
       msg = HM10.read();
       Serial.write(msg);
@@ -34,4 +52,43 @@ void loop(){
     if (Serial.available()) {
         HM10.write(Serial.read());       
     }
+    */
 }
+
+
+void TaskReadHM10(void *pvParameters){
+  (void) pvParameters;
+   while(1){
+    if (HM10.available()) {
+      msg = HM10.read();
+      xSemaphoreGive(xBinarySemaphore);
+    }
+  }
+}
+
+
+
+
+void TaskAction(void *pvParameters){
+  (void) pvParameters;
+   myServo.attach(3);
+   pinMode(8, OUTPUT);
+   while(1){
+    if (xSemaphoreTake(xBinarySemaphore, portMAX_DELAY)){
+      if(msg == '1'){
+        digitalWrite(8,HIGH);
+      }
+      else if(msg == '0'){
+        digitalWrite(8,LOW);
+      }
+      else if(msg == '2'){
+        myServo.write(180);
+      }
+      else if(msg == '3'){
+        myServo.write(0);
+      }
+    }
+  }
+}
+
+
